0791307470473d2d699367a5603177b4916bb1c9
Jacob <jacob.everist@gmail.com>
2023-03-29 11:36:24 -0700

    created simplified GnomeCode animation class with simple-to-use interface

--- test_shuffle.py
+++ test_shuffle.py
@@ -12,51 +12,15 @@ class GnomeCode(VGroup):
         super().__init__(**kwargs)
 
         self.num_bins = 32
-        #self.w = 3
         self.w = 10
         self.trackers = []
-        self.bins = VGroup()
-
-        # array of values from 0 to 1 for each textbox
-        self.trackers = [ValueTracker(0) for _ in range(self.num_bins)]
-        for i in range(self.num_bins):
-            self.trackers[i].set(index=i)
-            self.add(self.trackers[i])
-
+        self.bins = []
         self.rng = np.random.default_rng(0)
 
-    def set_code(self):
+        self.__init_array()
 
-
-        new_code = self.rng.choice([0, ] * (self.num_bins - self.w) + [1, ] * self.w, self.num_bins, replace=False,
-                              shuffle=True)
-        self.play(*[self.trackers[k].animate.set_value(new_code[k]) for k in range(self.num_bins)], run_time=0.5)
-        print("set:", new_code)
-
-    def permutate(self):
-
-        # shuffle indices
-        shuffled_indices = list(range(self.num_bins))
-        np.random.shuffle(shuffled_indices)
-
-        shuffled_bins = [self.bins[i] for i in shuffled_indices]
-
-        # move bins to their new index positions, but preserve index numbers
-        for i in range(self.num_bins):
-            bin = self.bins[i]
-            bin.generate_target()
-            bin.target.move_to(shuffled_bins[i].get_center())
-        self.play(*[MoveToTarget(bin) for bin in self.bins], run_time=0.5)
-
-
-    # updater function for textbox
     def __update_array(self):
-        """
-
-        :param varray: VGroup of textboxes
-        :param dt: float time increment
-        :return:
-        """
+        """updater member function called by inline-defined 'updater_func(mob)' in '__add_updater(self)' """
 
         for bin in self.bins:
             number = bin[0]
@@ -78,7 +42,7 @@ class GnomeCode(VGroup):
             square.set_fill(color=square_color, opacity=1).set_stroke(color=BLACK, opacity=1)
 
     def __add_updater(self) -> None:
-        """Attaches the value tracker updater function with the pointer."""
+        """Attaches the value tracker updater function to array animation"""
 
         def updater_func(mob: Mobject) -> None:
             self.__update_array()
@@ -86,59 +50,8 @@ class GnomeCode(VGroup):
         self.__updater_func = updater_func
         self.add_updater(self.__updater_func)
 
-
-
-class SquareShuffle(Scene):
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-        self.num_bins = 32
-        #self.w = 3
-        self.w = 10
-        self.trackers = []
-        self.bins = VGroup()
-
-        self.rng = np.random.default_rng(0)
-
-    def __update_array(self):
-        """
-
-        :param varray: VGroup of textboxes
-        :param dt: float time increment
-        :return:
-        """
-
-        for bin in self.bins:
-            number = bin[0]
-            square = bin[1]
-
-            # value trackers for bit value of element
-            val = number.tracker.get_value()
-
-            # change square background color
-            square_rgb = [(1.0 - val) for _ in range(3)]
-            square_color = rgb_to_color(square_rgb)
-
-            # change text value and color by "becoming" one of two different saved text mobjects
-            text_rgb = [val for _ in range(3)]
-            text_color = rgb_to_color(text_rgb)
-
-            # update colors
-            number.set_color(text_color)
-            square.set_fill(color=square_color, opacity=1).set_stroke(color=BLACK, opacity=1)
-
-    def __add_updater(self) -> None:
-        """Attaches the value tracker updater function with the pointer."""
-
-        def updater_func(mob: Mobject) -> None:
-            self.__update_array()
-
-        self.__updater_func = updater_func
-        self.bins.add_updater(self.__updater_func)
-
     def __init_array(self) -> None:
-        """ gnome code animation mobjects """
+        """ gnome code animation of mobjects """
 
         # array of values from 0 to 1 for each textbox
         self.trackers = [ValueTracker(0).set(index=_) for _ in range(self.num_bins)]
@@ -153,70 +66,61 @@ class SquareShuffle(Scene):
             vgroup = VGroup(num, square)
 
             # add to book-keeping list of bins
-            self.bins.add(vgroup)
+            self.bins.append(vgroup)
+            self.add(vgroup)
 
+        # add updater function to mobjects
         self.__add_updater()
 
-
     def set_code(self):
-
-        new_code = self.rng.choice([0, ] * (self.num_bins - self.w) + [1, ] * self.w, self.num_bins,
-                                   replace=False, shuffle=True)
-
-        #self.play(*[self.trackers[k].animate.set_value(new_code[k]) for k in range(self.num_bins)], run_time=0.5)
-        new_anims = [self.trackers[k].animate.set_value(new_code[k]) for k in range(self.num_bins)]
-
-        return new_anims
+        # activate w random bits
+        new_code = self.rng.choice([0, ] * (self.num_bins - self.w) + [1, ] * self.w, self.num_bins, replace=False, shuffle=True)
+        return [self.trackers[k].animate.set_value(new_code[k]) for k in range(self.num_bins)]
 
     def permutate(self):
 
-        # shuffle indices
-        shuffled_indices = list(range(self.num_bins))
-        np.random.shuffle(shuffled_indices)
-
-        shuffled_bins = [self.bins[i] for i in shuffled_indices]
+        # permutate indices
+        permutated_indices = list(range(self.num_bins))
+        np.random.shuffle(permutated_indices)
+        permutated_bins = [self.bins[i] for i in permutated_indices]
 
         # move bins to their new index positions, but preserve index numbers
         for i in range(self.num_bins):
             bin = self.bins[i]
             bin.generate_target()
-            bin.target.move_to(shuffled_bins[i].get_center())
-        #self.play(*[MoveToTarget(bin) for bin in self.bins], run_time=0.5)
-        new_anims = [MoveToTarget(bin) for bin in self.bins]
+            bin.target.move_to(permutated_bins[i].get_center())
 
-        return new_anims
+        return [MoveToTarget(bin) for bin in self.bins]
+
+
+class GnomeShuffle(Scene):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
     def construct(self):
 
         # frame configuration
         self.camera.background_color = GREY_C
 
-        # build array animation mobjects
-        self.__init_array()
-
-        # add value trackers to scene
-        self.add(*self.trackers)
-
-        # add gnome code array to scene
-        self.add(self.bins)
+        # initialize gnome code array animation mobject and add to scene
+        code = GnomeCode()
+        self.add(code)
 
         # group bins into array, arranged from left to right, and center it to screen
         num_cols = 8
-        self.bins.arrange_in_grid(cols=num_cols, buff=0.1).center()
-
+        code.arrange_in_grid(cols=num_cols, buff=0.1).center()
 
         for j in range(2):
 
-            # shuffle the array
-            self.play(*self.permutate(), run_time=0.5)
+            # permutate the array
+            self.play(*code.permutate(), run_time=0.5)
 
             # set encoding
-            self.play(*self.set_code(), run_time=0.5)
+            self.play(*code.set_code(), run_time=0.5)
 
             # rearrange grid layout
-            self.play(self.bins.animate.arrange_in_grid(cols=num_cols-1-j, buff=0.1).center(), run_time=0.5)
-            #.to_edge(LEFT).to_edge(UP)
+            self.play(code.animate.arrange_in_grid(cols=num_cols-1-j, buff=0.1).center(), run_time=0.5)
 
             self.wait(1)
-
 
