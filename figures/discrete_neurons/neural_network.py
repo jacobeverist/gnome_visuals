159cc2d0f78c23d39d17ed0d55f827372d803e72
jacobeverist <jacob.everist@gmail.com>
2023-04-16 01:37:48 -0700

    added neural layer rounded rectangle background

--- figures/discrete_neurons/neural_network.py
+++ figures/discrete_neurons/neural_network.py
@@ -8,9 +8,6 @@ np.set_printoptions(
         formatter={'bool': lambda bin_val: 'X' if bin_val else '-'})
 
 
-# manimgl code, needs to be modified to manim_community
-
-
 class NetworkMobject(VGroup):
     CONFIG = {
             "neuron_radius": 0.15,
@@ -23,15 +20,24 @@ class NetworkMobject(VGroup):
             "edge_stroke_width": 2,
             "edge_propogation_color": YELLOW,
             "edge_propogation_time": 1,
-            "max_shown_neurons": 16,
+            "max_shown_neurons": 8,
             "brace_for_large_layers": True,
             "average_shown_activation_of_large_layer": True,
             "include_output_labels": False,
     }
+    edge_groups = None
+    layers = None
+    output_labels = None
 
     def __init__(self, sizes, **kwargs):
+        # update local CONFIG
+        for k, v in {k: v for k, v in kwargs.items() if k in self.CONFIG}.items():
+            self.CONFIG[k] = kwargs.pop(k)
+
+        # send remaining kwargs to superclass
         VGroup.__init__(self, **kwargs)
 
+        # set CONFIG key-value pairs as member variables of this class instance
         self.__dict__.update(**self.CONFIG)
 
         # neural_networks needs to provide sizes array, integer size for each layer
@@ -39,6 +45,13 @@ class NetworkMobject(VGroup):
         self.layer_sizes = sizes
         self.add_neurons()
         self.add_edges()
+
+        # now put in rectangle backdrop
+        for layer in self.layers:
+            neurons = layer.neurons
+            neurons.add_background_rectangle(color=GREY_B, opacity=0.25,
+                                             stroke_opacity=1, stroke_width=3, stroke_color=GREY_B,
+                                             buff=1.5 * SMALL_BUFF, corner_radius=2.0 * self.neuron_radius)
 
     def add_neurons(self):
         layers = VGroup(*[
@@ -60,6 +73,7 @@ class NetworkMobject(VGroup):
                 Circle(
                         radius=self.neuron_radius,
                         stroke_color=self.neuron_stroke_color,
+                        # stroke_color=random_color(),
                         stroke_width=self.neuron_stroke_width,
                         fill_color=self.neuron_fill_color,
                         fill_opacity=0,
@@ -75,16 +89,14 @@ class NetworkMobject(VGroup):
         layer.neurons = neurons
         layer.add(neurons)
 
-        """
-        
         if size > n_neurons:
-            dots = OldTex("\\vdots")
+            dots = Text("⋮")
             dots.move_to(neurons)
             VGroup(*neurons[:len(neurons) // 2]).next_to(
-                dots, UP, MED_SMALL_BUFF
+                    dots, UP, MED_SMALL_BUFF
             )
             VGroup(*neurons[len(neurons) // 2:]).next_to(
-                dots, DOWN, MED_SMALL_BUFF
+                    dots, DOWN, MED_SMALL_BUFF
             )
             layer.dots = dots
             layer.add(dots)
@@ -94,7 +106,6 @@ class NetworkMobject(VGroup):
                 layer.brace = brace
                 layer.brace_label = brace_label
                 layer.add(brace, brace_label)
-        """
 
         return layer
 
@@ -115,8 +126,10 @@ class NetworkMobject(VGroup):
                 neuron1.get_center(),
                 neuron2.get_center(),
                 buff=self.neuron_radius,
+                # stroke_color=random_color(),
                 stroke_color=self.edge_color,
                 stroke_width=self.edge_stroke_width,
+                stroke_opacity=0.5,
         )
 
     def deactivate_layers(self):
@@ -129,38 +142,73 @@ class NetworkMobject(VGroup):
 
     def add_output_labels(self):
         self.output_labels = VGroup()
-        for n, neuron in enumerate(self.layers[-1].neurons):
-            # label = OldTex(str(n))
-            label = Text(str(n))
-            label.set_height(0.75 * neuron.get_height())
-            label.move_to(neuron)
-            label.shift(neuron.get_width() * RIGHT)
-            self.output_labels.add(label)
+        output_layer = self.layers[-1]
+        output_layer_size = self.layer_sizes[-1]
+        visible_layer_size = len(output_layer.neurons)
+
+        if output_layer_size > visible_layer_size:
+            first_neurons = output_layer.neurons[:visible_layer_size // 2]
+            last_neurons = output_layer.neurons[visible_layer_size // 2:]
+
+            neuron: Circle
+            for i, neuron in enumerate(first_neurons):
+                n = i
+                label = Text(str(n))
+                height = neuron.radius * 2
+                label.set(height=0.75 * height)
+                label.move_to(neuron)
+                label.shift(height * RIGHT)
+                self.output_labels.add(label)
+
+            for i, neuron in enumerate(last_neurons):
+                n = output_layer_size - len(last_neurons) + i
+                label = Text(str(n))
+                height = neuron.radius * 2
+                label.set(height=0.75 * height)
+                label.move_to(neuron)
+                label.shift(height * RIGHT)
+                self.output_labels.add(label)
+        else:
+            for n, neuron in enumerate(output_layer.neurons):
+                label = Text(str(n))
+                height = neuron.radius * 2
+                label.set(height=0.75 * height)
+                label.move_to(neuron)
+                label.shift(height * RIGHT)
+                self.output_labels.add(label)
+
         self.add(self.output_labels)
+
+    def remove_random_edges(self, prop=0.5):
+        for edge_group in self.edge_groups:
+            for edge in list(edge_group):
+                if np.random.random() < prop:
+                    edge_group.remove(edge)
 
 
 class NetworkScene(Scene):
     CONFIG = {
-            "layer_sizes": [8, 6, 6, 4],
+            # "layer_sizes": [8, 4, 8, 16, 4],
+            # "layer_sizes": [16, 8, 16, 32, 8],
+            "layer_sizes": [32, 16, 32, 64, 16],
             "network_mob_config": {},
     }
+    network_mob = None
 
+    def __init__(self, **kwargs):
+        # update local CONFIG
+        for k, v in {k: v for k, v in kwargs.items() if k in self.CONFIG}.items():
+            self.CONFIG[k] = kwargs.pop(k)
+
+        # send remaining kwargs to superclass
+        Scene.__init__(self, **kwargs)
+
+        # set CONFIG key-value pairs as member variables of this class instance
+        self.__dict__.update(**self.CONFIG)
 
     def construct(self):
-        self.setup()
-
-    def setup(self):
-        self.add_network()
-        self.remove_random_edges(prop=0.5)
-
-    def add_network(self):
-        # self.network = Network(sizes = self.layer_sizes)
-
-        self.network_mob = NetworkMobject([8, 4, 8, 16, 4])
+        self.network_mob = NetworkMobject(self.layer_sizes, include_output_labels=True, **self.network_mob_config)
+        self.network_mob.remove_random_edges(prop=0.8)
         self.add(self.network_mob)
 
-    def remove_random_edges(self, prop=0.9):
-        for edge_group in self.network_mob.edge_groups:
-            for edge in list(edge_group):
-                if np.random.random() < prop:
-                    edge_group.remove(edge)
+        # self.camera.background_color = GREY_C
