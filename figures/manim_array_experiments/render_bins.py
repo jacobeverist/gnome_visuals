c8e2cde6ecda06574226fe949ac65a50b27ea61c
jacobeverist <jacob.everist@gmail.com>
2023-06-01 09:23:26 -0700

    tetris brick splits and label is split as well

--- figures/manim_array_experiments/render_bins.py
+++ figures/manim_array_experiments/render_bins.py
@@ -14,6 +14,16 @@ import numpy as np
 #     and_parts.append(
 #             Polygon(*path, color=YELLOW, stroke_width=stroke_width, stroke_color=stroke_color, fill_opacity=1)
 #     )
+# NumberLine
+# DEFAULT_ARROW_TIP_LENGTH: float = 0.35
+# tip_width: float = DEFAULT_ARROW_TIP_LENGTH,
+# tip_height: float = DEFAULT_ARROW_TIP_LENGTH,
+# font_size: float = 36,
+#
+# from manim import config as global_config
+# config = global_config.copy()
+# config.frame_y_radius
+# config.frame_height
 
 class RectangleSeparationExample(Scene):
     def construct(self):
@@ -159,7 +169,7 @@ class BrickSplitExample(Scene):
         # self.add(*and_parts)
 
 
-class TetrisBricksExample(Scene):
+class SingleLevelTetrisBricksExample(Scene):
 
     # brick1 red, remains stationary
     # brick2 blue, falls from above
@@ -167,23 +177,67 @@ class TetrisBricksExample(Scene):
     # intersected components stop falling and remain on top of brick1
     # difference components keep falling and stop at same level as brick1
 
+    def create_textbox(self, string, *args, **kwargs):
+        result = VGroup()
+        box = Rectangle(*args, **kwargs)
+        text = Text(string).move_to(box.get_center())
+        result.add(box, text)
+        return result
+
+    def intersect(self, rvg1, rvg2):
+
+        poly1 = rvg1.submobjects[0]
+        poly2 = rvg2.submobjects[0]
+
+        text1 = rvg1.submobjects[1]
+
+        poly1_params = {
+                "fill_opacity": poly1.fill_opacity,
+                "stroke_width": poly1.stroke_width,
+                "fill_color": poly1.fill_color,
+                "stroke_color": poly1.stroke_color
+        }
+
+        and_bin = Intersection(poly1, poly2)
+        and_parts = [VGroup(Polygon(*path, **poly1_params))
+                     for path in and_bin.get_subpaths()
+                     ]
+        for part in and_parts:
+            part.add(text1.copy().move_to(part.get_center()))
+
+        return and_parts
+
+    def difference(self, rvg1, rvg2):
+
+        poly1 = rvg1.submobjects[0]
+        poly2 = rvg2.submobjects[0]
+
+        text1 = rvg1.submobjects[1]
+
+        poly1_params = {
+                "fill_opacity": poly1.fill_opacity,
+                "stroke_width": poly1.stroke_width,
+                "fill_color": poly1.fill_color,
+                "stroke_color": poly1.stroke_color
+        }
+
+        diff_bin = Difference(poly1, poly2)
+        diff_parts = [VGroup(Polygon(*path, **poly1_params))
+                      for path in diff_bin.get_subpaths()
+                      ]
+        for part in diff_parts:
+            part.add(text1.copy().move_to(part.get_center()))
+
+        return diff_parts
+
     def construct(self):
         stroke_width = 1
         stroke_color = WHITE
         # unit height
-        uh = 0.2
-        ut = 0.5
+        uh = 0.5
 
-        # NumberLine
-        # DEFAULT_ARROW_TIP_LENGTH: float = 0.35
-        # tip_width: float = DEFAULT_ARROW_TIP_LENGTH,
-        # tip_height: float = DEFAULT_ARROW_TIP_LENGTH,
-        # font_size: float = 36,
-        #
-        # from manim import config as global_config
-        # config = global_config.copy()
-        # config.frame_y_radius
-        # config.frame_height
+        # unit time
+        ut = 0.5
 
         nl = NumberLine(
                 x_range=[0, 1, 0.2],
@@ -206,59 +260,201 @@ class TetrisBricksExample(Scene):
         self.add(floor_line)
 
         # first brick on floor
-        brick1 = Rectangle(color=Color(hex=RED), width=4, height=uh, stroke_width=stroke_width,
-                           stroke_color=stroke_color,
-                           fill_opacity=0.8)
+        brick1 = self.create_textbox("0", color=Color(hex=RED), width=4, height=uh, stroke_width=stroke_width,
+                                     stroke_color=stroke_color, fill_opacity=0.8)
+
         brick1.next_to(floor_line, UP, buff=0)
         self.add(brick1)
 
         # second brick in sky
-        brick2 = Rectangle(color=Color(hex=BLUE), width=6, height=uh, stroke_width=stroke_width,
-                           stroke_color=stroke_color,
-                           fill_opacity=0.8)
+        brick2 = self.create_textbox("1", color=Color(hex=BLUE), width=6, height=uh, stroke_width=stroke_width,
+                                     stroke_color=stroke_color, fill_opacity=0.8)
         self.add(brick2)
 
-        # distance for brick2 to fall
+        # brick2 falls to next level
         target_point = brick1.get_top()
         point_to_align = brick2.get_bottom()
+
+        # compute fall distance to scale running time
         fall_dist1 = np.linalg.norm(UP * (target_point - point_to_align))
 
         # falling with ease_in_sine as rate_func
         self.play(brick2.animate(rate_func=rate_functions.ease_in_sine, run_time=ut * fall_dist1).shift(
                 (target_point - point_to_align) * UP))
 
-        # clone brick to perform bool operations
-        self.remove(brick2)
-        brick3 = brick2.copy()
-        brick3.move_to(brick1, coor_mask=UP)
+        # create temporary brick to perform diff/intersect comparisons
+        tmp_brick = brick2.copy()
+        tmp_brick.move_to(brick1, coor_mask=UP)
 
         # section that is stopped, stays above brick1
-        and_bin = Intersection(brick3, brick1)
-        and_parts = [
-                Polygon(*path, color=BLUE, stroke_width=stroke_width,
-                        stroke_color=stroke_color, fill_opacity=0.8).move_to(brick2, coor_mask=UP)
-                # stroke_color=stroke_color, fill_opacity=0.8).move_to(brick2, coor_mask=UP)
-                for path in and_bin.get_subpaths()
-        ]
-        self.add(*and_parts)
+        and_parts = self.intersect(tmp_brick, brick1)
+        for part in and_parts:
+            part.move_to(brick2, coor_mask=UP)
 
         # section that keeps falling, falls down to brick1 level
-        diff_bin = Difference(brick3, brick1)
-        diff_parts = [
-                Polygon(*path, color=BLUE, stroke_width=stroke_width,
-                        stroke_color=stroke_color, fill_opacity=0.8).move_to(brick2, coor_mask=UP)
-                for path in diff_bin.get_subpaths()
-        ]
-        self.add(*diff_parts)
+        diff_parts = self.difference(tmp_brick, brick1)
+        for part in diff_parts:
+            part.move_to(brick2, coor_mask=UP)
+
+        # remove old brick and replace with its separated parts
+        self.remove(brick2)
+        self.add(*and_parts, *diff_parts)
 
         # splitting parts fall to next level, rate_func is ease_out_sine on impact with floor
         target_point = brick1.get_bottom()
-        point_to_align = brick3.get_top()
+        point_to_align = tmp_brick.get_top()
+
+        # compute fall distance to scale running time
         fall_dist2 = np.linalg.norm(UP * (target_point - point_to_align))
 
         diff_anims = [
                 part.animate(rate_func=rate_functions.ease_out_sine, run_time=ut * fall_dist2).shift(
-                    (target_point - point_to_align) * UP) for
+                        (target_point - point_to_align) * UP) for
+                part in diff_parts]
+        self.play(*diff_anims)
+
+        # wait 1 second
+        self.play(Wait(1))
+
+
+class TetrisBricksExample(Scene):
+
+    @staticmethod
+    def create_textbox(string, *args, **kwargs):
+        result = VGroup()
+        box = Rectangle(*args, **kwargs)
+        text = Text(string).move_to(box.get_center())
+        result.add(box, text)
+        return result
+
+    @staticmethod
+    def intersect(rvg1, rvg2):
+
+        poly1 = rvg1.submobjects[0]
+        poly2 = rvg2.submobjects[0]
+
+        text1 = rvg1.submobjects[1]
+
+        poly1_params = {
+                "fill_opacity": poly1.fill_opacity,
+                "stroke_width": poly1.stroke_width,
+                "fill_color": poly1.fill_color,
+                "stroke_color": poly1.stroke_color
+        }
+
+        and_bin = Intersection(poly1, poly2)
+        and_parts = [VGroup(Polygon(*path, **poly1_params))
+                     for path in and_bin.get_subpaths()
+                     ]
+        for part in and_parts:
+            part.add(text1.copy().move_to(part.get_center()))
+
+        return and_parts
+
+    @staticmethod
+    def difference(rvg1, rvg2):
+
+        poly1 = rvg1.submobjects[0]
+        poly2 = rvg2.submobjects[0]
+
+        text1 = rvg1.submobjects[1]
+
+        poly1_params = {
+                "fill_opacity": poly1.fill_opacity,
+                "stroke_width": poly1.stroke_width,
+                "fill_color": poly1.fill_color,
+                "stroke_color": poly1.stroke_color
+        }
+
+        diff_bin = Difference(poly1, poly2)
+        diff_parts = [VGroup(Polygon(*path, **poly1_params))
+                      for path in diff_bin.get_subpaths()
+                      ]
+        for part in diff_parts:
+            part.add(text1.copy().move_to(part.get_center()))
+
+        return diff_parts
+
+    def construct(self):
+        stroke_width = 1
+        stroke_color = WHITE
+        # unit height
+        uh = 0.5
+
+        # unit time
+        ut = 0.5
+
+        nl = NumberLine(
+                x_range=[0, 1, 0.2],
+                length=10,
+                include_tip=True,
+                tip_width=0.25,
+                tip_height=0.2,
+                include_numbers=True,
+                font_size=32,
+                stroke_width=4
+        ).align_on_border(DOWN, buff=0.15)
+        self.add(nl)
+
+        floor_line = Rectangle(color=Color(hex=LIGHT_BROWN),
+                               width=global_config.frame_width,
+                               height=0.05,
+                               stroke_width=0,
+                               fill_opacity=0.8
+                               ).align_on_border(DOWN, buff=1)
+        self.add(floor_line)
+
+        # first brick on floor
+        brick1 = self.create_textbox("0", color=Color(hex=RED), width=4, height=uh, stroke_width=stroke_width,
+                                     stroke_color=stroke_color, fill_opacity=0.8)
+
+        brick1.next_to(floor_line, UP, buff=0)
+        self.add(brick1)
+
+        # second brick in sky
+        brick2 = self.create_textbox("1", color=Color(hex=BLUE), width=6, height=uh, stroke_width=stroke_width,
+                                     stroke_color=stroke_color, fill_opacity=0.8)
+        self.add(brick2)
+
+        # brick2 falls to next level
+        target_point = brick1.get_top()
+        point_to_align = brick2.get_bottom()
+
+        # compute fall distance to scale running time
+        fall_dist1 = np.linalg.norm(UP * (target_point - point_to_align))
+
+        # falling with ease_in_sine as rate_func
+        self.play(brick2.animate(rate_func=rate_functions.ease_in_sine, run_time=ut * fall_dist1).shift(
+                (target_point - point_to_align) * UP))
+
+        # create temporary brick to perform diff/intersect comparisons
+        tmp_brick = brick2.copy()
+        tmp_brick.move_to(brick1, coor_mask=UP)
+
+        # section that is stopped, stays above brick1
+        and_parts = self.intersect(tmp_brick, brick1)
+        for part in and_parts:
+            part.move_to(brick2, coor_mask=UP)
+
+        # section that keeps falling, falls down to brick1 level
+        diff_parts = self.difference(tmp_brick, brick1)
+        for part in diff_parts:
+            part.move_to(brick2, coor_mask=UP)
+
+        # remove old brick and replace with its separated parts
+        self.remove(brick2)
+        self.add(*and_parts, *diff_parts)
+
+        # splitting parts fall to next level, rate_func is ease_out_sine on impact with floor
+        target_point = brick1.get_bottom()
+        point_to_align = tmp_brick.get_top()
+
+        # compute fall distance to scale running time
+        fall_dist2 = np.linalg.norm(UP * (target_point - point_to_align))
+
+        diff_anims = [
+                part.animate(rate_func=rate_functions.ease_out_sine, run_time=ut * fall_dist2).shift(
+                        (target_point - point_to_align) * UP) for
                 part in diff_parts]
         self.play(*diff_anims)
 
