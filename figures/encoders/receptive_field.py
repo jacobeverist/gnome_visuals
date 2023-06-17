2fe6b984ea8ce07be9ecec8f5e4366484e69d1fc
jacobeverist <jacob.everist@gmail.com>
2023-06-16 19:39:41 -0700

    added better control of xrange and yaxis position

--- figures/encoders/receptive_field.py
+++ figures/encoders/receptive_field.py
@@ -9,12 +9,14 @@ class ExampleFunctionGraph(Scene):
         # gauss function parameters
         sigma = 0.5
         amp = 1
+        mu = 2.5
+
+        # xrange
+        xmin = -2.0
+        xmax = 4.0
 
         # shift origin to make y-axis at border of number plane
-        mu = 2.0
-
-        # distance from origin to end of x-range
-        xpad = mu
+        yaxis_shift = -xmin
 
         number_plane = NumberPlane(
                 background_line_style={
@@ -25,24 +27,25 @@ class ExampleFunctionGraph(Scene):
                 axis_config={"include_numbers": True},
                 x_axis_config={"include_numbers": False},
                 y_axis_config={"label_direction": LEFT},
-                x_range=[-xpad + mu, xpad + mu + 0.001, 1],
+                x_range=[xmin + yaxis_shift, xmax + yaxis_shift + 0.001, 1],
                 y_range=[0, 1.001, 0.25],
                 x_length=9,
                 y_length=7
         )
 
         # Shift x-axis labels by mu
-        label_indices = range(len(np.arange(-xpad + mu, xpad + mu + 0.001, 1)))
-        shifted_xlabels = np.arange(-xpad, xpad + 0.001, 1).astype(int)
+        label_indices = np.arange(xmin + yaxis_shift, xmax + yaxis_shift + 0.001, 1).astype(int)
+        shifted_xlabels = np.arange(xmin, xmax + 0.001, 1).astype(int)
         label_dict = {k: v for (k, v) in zip(label_indices, shifted_xlabels)}
         number_plane.x_axis.add_labels(label_dict)
         self.add(number_plane)
 
         # Plot gaussian function
         def gauss_func(t):
-            return amp * np.exp(-(t - mu) * (t - mu) / (2 * sigma * sigma))
+            t_shift = t - yaxis_shift
+            return amp * np.exp(-(t_shift - mu) * (t_shift - mu) / (2 * sigma * sigma))
         gauss_graph = number_plane.plot(gauss_func, color=RED, use_smoothing=False,
-                                        x_range=[-xpad + mu, xpad + mu + 0.001, 0.001])
+                                        x_range=[xmin + yaxis_shift, xmax + yaxis_shift + 0.001, 0.001])
         self.add(gauss_graph)
 
         # print(number_plane.coords_to_point([LEFT, ]))
