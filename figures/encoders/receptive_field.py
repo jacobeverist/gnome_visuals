b652f5afd3a330695f036e9c6c3789fdac7593e6
jacobeverist <jacob.everist@gmail.com>
2023-06-16 20:08:21 -0700

    added axis tips and scaled numberplane to fit camera frame

--- figures/encoders/receptive_field.py
+++ figures/encoders/receptive_field.py
@@ -1,4 +1,5 @@
 from manim import *
+from manim.mobject.geometry.tips import *
 
 # appending a path
 sys.path.append("../../")
@@ -13,24 +14,39 @@ class ExampleFunctionGraph(Scene):
 
         # xrange
         xmin = -2.0
-        xmax = 4.0
+        xmax = 6.0
 
         # shift origin to make y-axis at border of number plane
         yaxis_shift = -xmin
 
+        # camera frame parameters
+        frame_height = self.camera.frame_height
+        frame_width = self.camera.frame_width
+
+        # size of numberplane scaled to fit camera frame
+        x_frame_pad = 0.7
+        y_frame_pad = 0.5
+        x_length = frame_width - 2*x_frame_pad
+        y_length = frame_height - 2*y_frame_pad
+
         number_plane = NumberPlane(
                 background_line_style={
                         "stroke_color": TEAL,
-                        "stroke_width": 4,
-                        "stroke_opacity": 0.6
+                        "stroke_width": 2,
+                        "stroke_opacity": 0.4
                 },
-                axis_config={"include_numbers": True},
-                x_axis_config={"include_numbers": False},
-                y_axis_config={"label_direction": LEFT},
+
+                # x-axis config
+                x_axis_config={"include_numbers": False, "include_tip": True,
+                               "tip_shape": StealthTip, "tip_height": 0.15},
                 x_range=[xmin + yaxis_shift, xmax + yaxis_shift + 0.001, 1],
+                x_length=x_length,
+
+                # y-axis config
+                y_axis_config={"label_direction": LEFT, "include_numbers": True, "include_tip": True,
+                               "tip_shape": StealthTip, "tip_height": 0.15},
                 y_range=[0, 1.001, 0.25],
-                x_length=9,
-                y_length=7
+                y_length=y_length
         )
 
         # Shift x-axis labels by mu
@@ -44,6 +60,7 @@ class ExampleFunctionGraph(Scene):
         def gauss_func(t):
             t_shift = t - yaxis_shift
             return amp * np.exp(-(t_shift - mu) * (t_shift - mu) / (2 * sigma * sigma))
+
         gauss_graph = number_plane.plot(gauss_func, color=RED, use_smoothing=False,
                                         x_range=[xmin + yaxis_shift, xmax + yaxis_shift + 0.001, 0.001])
         self.add(gauss_graph)
@@ -51,4 +68,3 @@ class ExampleFunctionGraph(Scene):
         # print(number_plane.coords_to_point([LEFT, ]))
         # print(number_plane.y_axis.number_to_point(-1.0))
         # print(LEFT * number_plane.x_axis.number_to_point(10.0))
-
